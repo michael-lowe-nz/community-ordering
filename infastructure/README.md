2b30a2c8fece94b36a86516eb88115fe5427b98d
Michael Lowe <lowe.michael.nz@gmail.com>
2025-03-08 22:22:26 +1300

    Make the Laravel app the main thing, and the CDK app lives in a subdirectory

---
+++ infastructure/README.md
@@ -0,0 +1,11 @@
+# Laravel.Docker.CDK.DevContainer.Fargate Starter Kit
+
+
+```
+[profile personal-dev]
+sso_start_url   = https://d-976759b382.awsapps.com/start/
+sso_region      = ap-southeast-2
+sso_account_id  = 476203294330 
+sso_role_name   = AWSAdministratorAccess
+region          = ap-southeast-2
+```
---
+++ infastructure/src/main.ts
@@ -0,0 +1,39 @@
+import { App, aws_ec2, aws_ecs, aws_ecs_patterns, Stack, StackProps } from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import path from 'path';
+
+export class ApplicationStack extends Stack {
+  constructor(scope: Construct, id: string, props: StackProps = {}) {
+    super(scope, id, props);
+
+     // define resources here...
+     const vpc = new aws_ec2.Vpc(this, 'Network', {
+      maxAzs: 2,
+    });
+
+    const cluster = new aws_ecs.Cluster(this, 'Cluster', {
+      vpc: vpc,
+    });
+
+    // Create a load-balanced Fargate service and make it public
+    new aws_ecs_patterns.ApplicationLoadBalancedFargateService(this, 'Laravel', {
+      cluster: cluster,
+      taskImageOptions: { image: aws_ecs.ContainerImage.fromAsset(path.join(__dirname, '../laravel-app/sampleApp')) },
+      publicLoadBalancer: true, // Default is true
+      enableExecuteCommand: true
+    });
+  }
+}
+
+// for development, use account/region from cdk cli
+const devEnv = {
+  account: process.env.CDK_DEFAULT_ACCOUNT,
+  region: process.env.CDK_DEFAULT_REGION,
+};
+
+const app = new App();
+
+new ApplicationStack(app, 'laravel-starter-dev', { env: devEnv });
+// new MyStack(app, 'laravel-starter-prod', { env: prodEnv });
+
+app.synth();
