bf544215aee96d6f011e862d4ed33cdf5802fe0d
Michael Lowe <lowe.michael.nz@gmail.com>
2025-03-08 01:20:35 +0000

    Create laravel app

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,26 @@
-import { App, Stack, StackProps } from 'aws-cdk-lib';
+import { App, aws_ec2, aws_ecs, aws_ecs_patterns, Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import path from 'path';
 
-export class MyStack extends Stack {
+export class ApplicationStack extends Stack {
   constructor(scope: Construct, id: string, props: StackProps = {}) {
     super(scope, id, props);
 
-    // define resources here...
+     // define resources here...
+     const vpc = new aws_ec2.Vpc(this, 'Network', {
+      maxAzs: 2,
+    });
+
+    const cluster = new aws_ecs.Cluster(this, 'Cluster', {
+      vpc: vpc,
+    });
+
+    // Create a load-balanced Fargate service and make it public
+    new aws_ecs_patterns.ApplicationLoadBalancedFargateService(this, 'Laravel', {
+      cluster: cluster,
+      taskImageOptions: { image: aws_ecs.ContainerImage.fromAsset(path.join(__dirname, '../laravel-app')) },
+      publicLoadBalancer: true, // Default is true
+    });
   }
 }
 
@@ -17,7 +32,7 @@ const devEnv = {
 
 const app = new App();
 
-new MyStack(app, 'laravel-starter-dev', { env: devEnv });
+new ApplicationStack(app, 'laravel-starter-dev', { env: devEnv });
 // new MyStack(app, 'laravel-starter-prod', { env: prodEnv });
 
 app.synth();
