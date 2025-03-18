5626ff305c6cf6010aa0ffa602a23a16ae5877df
Michael Lowe <lowe.michael.nz@gmail.com>
2025-03-18 05:29:17 +0000

    Scaffold with Jetstream and Livewire

--- routes/web.php
+++ routes/web.php
@@ -13,3 +13,12 @@ Route::get('/restaurant/{restaurant}', [RestaurantController::class, 'show']);
 Route::get('/health-check', function () {
     return response('OK', 200);
 });
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
