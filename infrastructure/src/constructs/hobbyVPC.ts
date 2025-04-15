b2ccd4bf3c916670227f5f963530f8d7f9f304aa
Michael Lowe <lowe.michael.nz@gmail.com>
2025-04-15 21:44:53 +1200

    Infra changes

--- infrastructure/src/constructs/hobbyVPC.ts
+++ infrastructure/src/constructs/hobbyVPC.ts
@@ -1,15 +1,26 @@
 import { Construct } from 'constructs';
-import { aws_ec2 as ec2, aws_iam as iam } from 'aws-cdk-lib';
+import { aws_lambda_nodejs, aws_ec2 as ec2, aws_iam as iam, StackProps } from 'aws-cdk-lib';
 import { FckNatInstanceProvider } from 'cdk-fck-nat';
-import { Peer, Port } from 'aws-cdk-lib/aws-ec2';
 
+interface HobbyVPCProps extends StackProps {
+    createTestInstance: boolean;
+    /**
+     * Turn off at night
+     * @default false 
+     * Specifies whether the NAT instances, NAT gateways, and EC2 instances should be turned off at night.
+     */
+    turnOffAtNight: boolean;
+}
 export class HobbyVPC extends Construct {
     public readonly vpc: ec2.Vpc;
     public readonly natInstance: FckNatInstanceProvider;
-    public readonly testInstance: ec2.Instance;
+    public readonly testInstance?: ec2.Instance;
+    public readonly turnOffAtNight: boolean;
 
-    constructor(scope: Construct, id: string) {
+    constructor(scope: Construct, id: string, props: HobbyVPCProps) {
         super(scope, id);
+
+        this.turnOffAtNight = props.turnOffAtNight;
 
         // Create NAT instance using cdk-fck-nat
         this.natInstance = new FckNatInstanceProvider({
@@ -19,23 +30,23 @@ export class HobbyVPC extends Construct {
         // Create the VPC with 1 AZ, 1 public subnet, and 1 private subnet
         this.vpc = new ec2.Vpc(this, 'VPC', {
             maxAzs: 2,
+            ipAddresses: ec2.IpAddresses.cidr('10.0.0.0/26'),
             subnetConfiguration: [
                 {
                     name: 'Public',
                     subnetType: ec2.SubnetType.PUBLIC,
-                    cidrMask: 24,
+                    cidrMask: 28,
                 },
                 {
                     name: 'Private',
                     subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS,
-                    cidrMask: 24,
+                    cidrMask: 28,
                 },
             ],
             natGatewayProvider: this.natInstance,
         });
 
-        this.natInstance.securityGroup.addIngressRule(Peer.ipv4(this.vpc.vpcCidrBlock), Port.allTraffic());
-
+        this.natInstance.securityGroup.addIngressRule(ec2.Peer.ipv4(this.vpc.vpcCidrBlock), ec2.Port.allTraffic());
 
         // Create security group for EC2 Instance Connect Endpoint
         const eicSecurityGroup = new ec2.SecurityGroup(this, 'EC2InstanceConnectSecurityGroup', {
@@ -44,69 +55,80 @@ export class HobbyVPC extends Construct {
             allowAllOutbound: true,
         });
 
-        // Create EC2 Instance Connect Endpoint
-        new ec2.CfnInstanceConnectEndpoint(this, 'EC2InstanceConnectEndpoint', {
-            subnetId: this.vpc.privateSubnets[0].subnetId,
-            securityGroupIds: [eicSecurityGroup.securityGroupId],
-        });
+        if(props.createTestInstance) {
+            // Create EC2 Instance Connect Endpoint
+            new ec2.CfnInstanceConnectEndpoint(this, 'EC2InstanceConnectEndpoint', {
+                subnetId: this.vpc.privateSubnets[0].subnetId,
+                securityGroupIds: [eicSecurityGroup.securityGroupId],
+            });
+    
+            // Create security group for test instance
+            const testSecurityGroup = new ec2.SecurityGroup(this, 'TestSecurityGroup', {
+                vpc: this.vpc,
+                description: 'Security group for test instance',
+                allowAllOutbound: true,
+            });
+    
+            // Allow SSH access from the EC2 Instance Connect Endpoint using L2 construct
+            testSecurityGroup.addIngressRule(
+                ec2.SecurityGroup.fromSecurityGroupId(this, 'EICSecurityGroupReference', eicSecurityGroup.securityGroupId),
+                ec2.Port.tcp(22),
+                'Allow SSH from EC2 Instance Connect Endpoint'
+            );
+    
+            // Create test instance with connectivity test script
+            const testUserData = ec2.UserData.forLinux();
+            testUserData.addCommands(
+                'yum update -y',
+                'yum install -y amazon-cloudwatch-agent curl',
+    
+                // Create a test script
+                'cat << EOF > /home/ec2-user/test-connectivity.sh',
+                '#!/bin/bash',
+                'echo "Testing connectivity..."',
+                'curl -s --max-time 5 http://checkip.amazonaws.com || echo "Failed to reach internet"',
+                'echo "Testing DNS resolution..."',
+                'nslookup amazon.com || echo "Failed to resolve DNS"',
+                'EOF',
+    
+                'chmod +x /home/ec2-user/test-connectivity.sh',
+    
+                // Run the test immediately and save to a log file
+                '/home/ec2-user/test-connectivity.sh > /home/ec2-user/connectivity-test.log 2>&1'
+            );
+    
+            this.testInstance = new ec2.Instance(this, 'TestInstance', {
+                vpc: this.vpc,
+                vpcSubnets: {
+                    subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS,
+                },
+                instanceType: ec2.InstanceType.of(ec2.InstanceClass.T3, ec2.InstanceSize.NANO),
+                machineImage: new ec2.AmazonLinuxImage({
+                    generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2,
+                    cpuType: ec2.AmazonLinuxCpuType.X86_64,
+                }),
+                securityGroup: testSecurityGroup,
+                userData: testUserData,
+                requireImdsv2: true,
+                instanceName: `${this.node.path}-test-instance`,
+            });
+    
+            /// Add the SSMManagedCore policy to the instance role
+            this.testInstance.role.addManagedPolicy(
+                iam.ManagedPolicy.fromAwsManagedPolicyName('EC2InstanceConnect')
+            );
+        }
 
-        // Create security group for test instance
-        const testSecurityGroup = new ec2.SecurityGroup(this, 'TestSecurityGroup', {
-            vpc: this.vpc,
-            description: 'Security group for test instance',
-            allowAllOutbound: true,
-        });
-
-        // Allow SSH access from the EC2 Instance Connect Endpoint using L2 construct
-        testSecurityGroup.addIngressRule(
-            ec2.SecurityGroup.fromSecurityGroupId(this, 'EICSecurityGroupReference', eicSecurityGroup.securityGroupId),
-            ec2.Port.tcp(22),
-            'Allow SSH from EC2 Instance Connect Endpoint'
-        );
-
-        // Create test instance with connectivity test script
-        const testUserData = ec2.UserData.forLinux();
-        testUserData.addCommands(
-            'yum update -y',
-            'yum install -y amazon-cloudwatch-agent curl',
-
-            // Create a test script
-            'cat << EOF > /home/ec2-user/test-connectivity.sh',
-            '#!/bin/bash',
-            'echo "Testing connectivity..."',
-            'curl -s --max-time 5 http://checkip.amazonaws.com || echo "Failed to reach internet"',
-            'echo "Testing DNS resolution..."',
-            'nslookup amazon.com || echo "Failed to resolve DNS"',
-            'EOF',
-
-            'chmod +x /home/ec2-user/test-connectivity.sh',
-
-            // Run the test immediately and save to a log file
-            '/home/ec2-user/test-connectivity.sh > /home/ec2-user/connectivity-test.log 2>&1'
-        );
-
-        this.testInstance = new ec2.Instance(this, 'TestInstance', {
-            vpc: this.vpc,
-            vpcSubnets: {
-                subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS,
-            },
-            instanceType: ec2.InstanceType.of(ec2.InstanceClass.T3, ec2.InstanceSize.NANO),
-            machineImage: new ec2.AmazonLinuxImage({
-                generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2,
-                cpuType: ec2.AmazonLinuxCpuType.X86_64,
-            }),
-            securityGroup: testSecurityGroup,
-            userData: testUserData,
-            requireImdsv2: true,
-            instanceName: `${this.node.path}-test-instance`,
-        });
-
-        /// Add the SSMManagedCore policy to the instance role
-        this.testInstance.role.addManagedPolicy(
-            iam.ManagedPolicy.fromAwsManagedPolicyName('EC2InstanceConnect')
-        );
-
-        // An event bridge rule that triggers a command to delete the whole VPC every day at 1AM
-
+        if (this.turnOffAtNight) {
+            // Create a Lambda function to scale the NAT instance ASG down to 0 instances at night
+            new aws_lambda_nodejs.NodejsFunction(this, 'ScaleDownNatInstance', {
+                entry: 'src/lambda/scale-down-nat-instance.ts',
+                handler: 'handler',
+                environment: {
+                    NAT_INSTANCE_ASG_NAMES: this.natInstance.autoScalingGroups.map((asg) => asg.autoScalingGroupName).join(','),
+                },
+            });
+        }
+        
     }
 }
--- infrastructure/src/main.ts
+++ infrastructure/src/main.ts
@@ -18,13 +18,31 @@ import { GitHubActionRole } from "cdk-pipelines-github";
 export class NetworkStack extends Stack {
   cluster: aws_ecs.Cluster;
   vpc: aws_ec2.Vpc;
+  hostedZone: aws_route53.HostedZone;
+  certificate: aws_certificatemanager.Certificate;
   constructor(scope: Construct, id: string, props: StackProps = {}) {
     super(scope, id, props);
 
-    this.vpc = new HobbyVPC(this, 'Network').vpc;
+    // Create the Hobby VPC, which has a NAT instance, a public subnet, a private subnet, a test instance, and a Lambda that keeps that NAT instances
+    // off after hours to save money.
+    this.vpc = new HobbyVPC(this, 'Network', {
+      createTestInstance: false,
+      turnOffAtNight: true,
+    }).vpc;
     
     this.cluster = new aws_ecs.Cluster(this, 'Cluster', {
       vpc: this.vpc,
+    });
+
+    // Add a hosted zone for orderfood.site
+    this.hostedZone = new aws_route53.HostedZone(this, 'HostedZone', {
+      zoneName: 'orderfood.site',
+    });
+
+    // Create an ACM certificate for the domain
+    this.certificate = new aws_certificatemanager.Certificate(this, 'Certificate', {
+      domainName: 'orderfood.site',
+      validation: aws_certificatemanager.CertificateValidation.fromDns(this.hostedZone),
     });
   }
 }
@@ -32,22 +50,13 @@ export class NetworkStack extends Stack {
 interface ApplicationStackProps extends StackProps {
   vpc: aws_ec2.Vpc;
   cluster: aws_ecs.Cluster;
+  hostedZone: aws_route53.HostedZone;
+  certificate: aws_certificatemanager.Certificate;
 }
 
 export class ApplicationStack extends Stack {
   constructor(scope: Construct, id: string, props: ApplicationStackProps) {
     super(scope, id, props);
-
-    // Add a hosted zone for orderfood.site
-    const hostedZone = new aws_route53.HostedZone(this, 'HostedZone', {
-      zoneName: 'orderfood.site',
-    });
-
-    // Create an ACM certificate for the domain
-    const certificate = new aws_certificatemanager.Certificate(this, 'Certificate', {
-      domainName: 'orderfood.site',
-      validation: aws_certificatemanager.CertificateValidation.fromDns(hostedZone),
-    });
 
     const googleAPIKey = aws_secretsmanager.Secret.fromSecretNameV2(this, 'GooglePlacesAPIKey', 'GooglePlacesAPIKey');
 
@@ -56,10 +65,9 @@ export class ApplicationStack extends Stack {
       circuitBreaker: { rollback: true },
       minHealthyPercent: 0,
       maxHealthyPercent: 100,
-      certificate,
-      // sslPolicy: Ssl.RECOMMENDED,
+      certificate: props.certificate,
       domainName: 'orderfood.site',
-      domainZone: hostedZone,
+      domainZone: props.hostedZone,
       redirectHTTP: true,
       protocol: aws_elasticloadbalancingv2.ApplicationProtocol.HTTPS,
       taskImageOptions: {
@@ -91,7 +99,7 @@ export class ApplicationStack extends Stack {
 
     // Create DNS records for the load balancer
     new aws_route53.ARecord(this, 'AliasRecord', {
-      zone: hostedZone,
+      zone: props.hostedZone,
       target: aws_route53.RecordTarget.fromAlias(new aws_route53_targets.LoadBalancerTarget(ecsTask.loadBalancer)),
       recordName: 'orderfood.site',
     });
@@ -157,7 +165,13 @@ const devEnv = {
 const app = new App();
 
 const networkStack = new NetworkStack(app, 'CommunityOrdering-Network', { env: devEnv });
-new ApplicationStack(app, 'CommunityOrdering-Application', { env: devEnv, vpc: networkStack.vpc, cluster: networkStack.cluster });
+new ApplicationStack(app, 'CommunityOrdering-Application', {
+  env: devEnv,
+  vpc: networkStack.vpc,
+  cluster: networkStack.cluster,
+  hostedZone: networkStack.hostedZone,
+  certificate: networkStack.certificate
+});
 
 if (process.env.SETUP_ONLY) {
   new SupportStack(app, 'CommunityOrdering-Support', { env: devEnv });
