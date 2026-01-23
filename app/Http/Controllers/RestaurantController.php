11f21e11c526f59fca2a634a509a30153d1e2a7c
Michael Lowe <lowe.michael.nz@gmail.com>
2026-01-23 08:47:53 +0000

    Fix broken debugging

--- app/Http/Controllers/RestaurantController.php
+++ app/Http/Controllers/RestaurantController.php
@@ -10,10 +10,9 @@ class RestaurantController
     public function index()
     {
         $restaurants = Restaurant::orderBy('name', 'asc')->get();
-        dlfk;
         return view('restaurants.index', compact('restaurants'));
     }
-
+        
     public function show(Restaurant $restaurant)
     {
         return view('restaurants.show', compact('restaurant'));
