54c692418992525bb17f010953c483ea94bdf83a
Michael Lowe <lowe.michael.nz@gmail.com>
2026-01-24 22:03:35 +0000

    Pull order component out of order.show view

--- resources/views/components/navigation.blade.php
+++ resources/views/components/navigation.blade.php
@@ -19,9 +19,6 @@
                     <a href="{{ url('/') }}" class="inline-flex items-center px-1 pt-1 border-b-2 {{ request()->is('/') ? 'border-orange-500 text-gray-900' : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300' }} text-sm font-medium leading-5 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
                         {{ __('Home') }}
                     </a>
-                    <a href="/restaurant" class="inline-flex items-center px-1 pt-1 border-b-2 {{ request()->is('restaurant*') ? 'border-orange-500 text-gray-900' : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300' }} text-sm font-medium leading-5 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
-                        {{ __('Restaurants') }}
-                    </a>
                 </div>
             </div>
 
--- resources/views/order/show.blade.php
+++ resources/views/order/show.blade.php
@@ -7,7 +7,7 @@
 </x-slot>
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            
+            <x-order-receipt :orderItems="$orderItems"/>
         </div>
     </div>
     <div class="py-12">
@@ -15,10 +15,12 @@
             <h1>Add to order</h1>
             <form action="{{ route('restaurants.orders.addToOrder', [$restaurant, $order]) }}" method="POST">
                 @csrf
-                <label>Item Name:</label>
-                <input type="text" name="name" placeholder="Item Name" class="border border-gray-300 rounded-md p-2 w-full mb-4" autofocus>
-                <label>Quantity:</label>
-                <input type="number" name="quantity" placeholder="quantity" value="1" class="border border-gray-300 rounded-md p-2 w-full mb-4">
+                <div class="mb-4 flex flex-col">
+                    <label>Item Name:</label>
+                    <input type="text" name="name" placeholder="Item Name" class="border border-gray-300 rounded-md p-2 w-full mb-4" autofocus>
+                    <label>Quantity:</label>
+                    <input type="number" name="quantity" placeholder="quantity" value="1" class="border border-gray-300 rounded-md p-2 w-full mb-4">
+                </div>
                 <button type="submit" class="text-white bg-orange-600 hover:bg-orange-700 px-4 py-2 rounded-md font-semibold transition-colors duration-200">Add</button>
             </form>
         </div>
